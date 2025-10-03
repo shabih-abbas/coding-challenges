32e6a3f40f83eee07fdcfdb1601c173548553955
Shabih Abbas <shabihabbas09@gmail.com>
2025-10-03 16:32:31 +0500

    Create integerClassification.js

---
+++ numbers/integerClassification.js
@@ -0,0 +1,38 @@
+/*
+Rosetta Code Challenge # 5
+Problem: Implement a function that calculates how many of the integers from 1 to num (inclusive) are in each of the three classes. Output the result as an array in the following format [deficient, perfect, abundant]
+*/
+function getDPA(num) {
+  var classification = [0, 0, 0];
+  for (let n = 1; n <= num; n++) {
+    let divisorSum = getSumOfFactors(n)
+    let classed = divisorSum < n ? 0 : divisorSum == n ? 1 : 2
+    classification[classed]++
+  }
+  return classification
+  function getSumOfFactors(num) {
+    var sumOfFactors = 0
+    var limit = Math.sqrt(num);
+    for (let n = 1; n <= limit; n++) {
+      if (num % n == 0) {
+        sumOfFactors += (n != num) ? n : 0
+        let otherFactor = num / n
+        if (otherFactor != n && otherFactor != num) sumOfFactors += otherFactor
+      }
+    }
+    return sumOfFactors;
+  }
+}
+//tests
+console.log(
+  JSON.stringify(getDPA(5000)) == 
+  JSON.stringify([3758, 3, 1239])
+  )
+console.log(
+  JSON.stringify(getDPA(10000)) == 
+  JSON.stringify([7508, 4, 2488])
+  )
+console.log(
+  JSON.stringify(getDPA(20000)) == 
+  JSON.stringify([15043, 4, 4953])
+  )
